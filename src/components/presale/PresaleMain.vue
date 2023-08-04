ef98130295824715676abb8b629a2f2aa7fb38cd
Martin <martin.kuijs@gmail.com>
2023-08-04 16:26:44 +0700

    Setup Presale component

---
+++ src/components/presale/PresaleMain.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="presale">
+    <v-responsive style="background-color: #000;">
+      
+      <v-card theme="dark" class="mt-16 mb-16" height="100%">
+
+       <v-row :class="isMobileDevice ? 'mt-12 ml-2 mr-2' : 'mt-12'" :align="center">
+        <v-col cols="12" md="2" :align="center" v-if="!isMobileDevice">
+        </v-col>
+
+        <v-col cols="12" md="8" :align="center">
+          <div class="text-h4 text-center ma-2">Presale Home</div>
+        </v-col>
+
+        <v-col cols="12" md="2" :align="center" v-if="!isMobileDevice">
+        </v-col>
+
+       </v-row>
+
+      </v-card>
+    </v-responsive>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { scroller } from 'vue-scrollto/src/scrollTo'
+export default {
+  name: 'Presale',
+  props: {
+    isMobileDevice: Boolean,
+    dark: Boolean,
+    windowWidth: Number,
+    windowHeight: Number,
+    drawer: Boolean
+  },
+  data: () => ({
+    loading: false,
+  }),
+  components: {
+  },
+  computed: {
+
+  },
+  watch: {
+  },
+  created() {
+    // this.currentUser = firebase.auth().currentUser;
+    this.scrollToTop()
+    this.init()
+  },
+  methods: {
+    init () {
+     
+    },
+    scrollToTop () {
+      const firstScrollTo = scroller();
+      this.scrollClicked = true
+      setTimeout(() => {
+        firstScrollTo('#presale', 500, { offset: -64 });
+      }, 200);
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  .homeText {
+    font-family: 'Gruppo';
+    font-size: 40px;
+    line-height: 1em;
+    color: #FFF;
+  }
+  .homeTextSmall {
+    font-family: 'Gruppo';
+    font-size: 28px;
+    line-height: 1em;
+    color: #FFF;
+  }
+  .homeTextSmaller {
+    font-family: 'Gruppo';
+    font-size: 24px;
+    line-height: 1em;
+    color: #FFF;
+    margin-top: 16px;
+  }
+  .cardTextTitle {
+    font-family: 'Saira';
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .cardTextCreateSmall {
+    font-family: 'Jura';
+    font-size: 18px;
+  }
+  .gallery {
+    font-family: 'Gruppo';
+    font-size: 45px;
+    line-height: 1em;
+    color: #FFF;
+  }
+  .v-card--reveal {
+    bottom: 0;
+    opacity: 1 !important;
+    position: absolute;
+    width: 100%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -152,6 +152,14 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Roadmap.vue'),
       },
+      {
+        path: '/presale',
+        name: 'Presale',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/PresaleMain.vue'),
+      },
       // Admin Modules
       {
         path: '/admin/nfts',
