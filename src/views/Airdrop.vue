7d9ed13531360d82bbe77b98c9b4ac7fa40f25c0
mgportofolio <gunawan.michael20@gmail.com>
2023-08-22 08:29:53 +0700

    Create airdrop 01

---
+++ src/views/Airdrop.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container style="width:100%;height:100%;" fluid class="mx-0 px-0 mb-0 pb-0 mt-16">
+        <v-row style="width:100%;height:100%;" class="mx-0 px-0 mb-0 pb-0">
+            <v-col cols="12" class="mx-0 px-0 mb-0 pb-0">
+                <iframe src="https://gleam.io/203UF/free-token-airdrop" frameBorder="0" allowfullscreen style="width:100%;height:100%;overflow: hidden;"></iframe>
+            </v-col>            
+        </v-row>
+    </v-container>  
+</template>
+  
+<script setup>
+</script>
+  
