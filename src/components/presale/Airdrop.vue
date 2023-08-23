3f4afef0ddf2cad5d96513b5cec1e6c97e919a2a
Martin <martin.kuijs@gmail.com>
2023-08-23 11:40:36 +0700

    SOCIAL MEDIA EMAS TOKENS LUCKY DRAW 02

---
+++ src/components/presale/Airdrop.vue
@@ -0,0 +1,56 @@
+<template>
+    <div style="width:100%;background-color: #F3E5F5;" fluid>
+        <!-- <v-row :class="isMobileDevice ? 'mt-12 ml-2 mr-2' : 'mt-4'">
+            <v-col cols="12" :align="'center'">
+                <div class="text-h4 ma-2 text-purple-darken-3">SOCIAL MEDIA EMAS TOKENS LUCKY DRAW</div>
+                <div class="text-h6 ml-4 mr-4 mt-4" :style="isMobileDevice ? 'text-align: justify;' : 'padding-left:10%;padding-right:10%'">
+                    Social media listings lucky draw EMAS tokens to the lucky top 50 handles joining and posting on any of our social medias will be drawn and airdropped no need to hold EMAS tokens to join.<br><br>
+                    Simply post something thats cool about MemeMaster and get people to like your comments, and you enter the lucky draw airdrop after the listing. The more likes you get and the more channels you post in, the more entries into the lucky draw.<br><br>
+                    All wining handles will be posted in our social media platforms after the airdrop
+                </div>
+            </v-col>            
+        </v-row> -->
+        <v-row style="width:100%;margin: 0px" :class="isMobileDevice ? 'mt-12' : 'mt-4'">
+            <v-col cols="12" class="pl-4 pr-4">
+                <iframe src="https://gleam.io/203UF/free-token-airdrop" 
+                        frameBorder="0" 
+                        allowfullscreen 
+                        scrolling="no"
+                        :style="isMobileDevice ? 'width:100%;height:1200px;overflow: hidden;' : 'width:100%;height:840px;overflow: hidden;'"
+                >
+                </iframe>
+            </v-col>            
+        </v-row>
+    </div>  
+</template>
+  
+<script>
+// @ is an alias to /src
+import { scroller } from 'vue-scrollto/src/scrollTo'
+export default {
+  name: 'SOCIAL MEDIA EMAS TOKENS LUCKY DRAW',
+  props: {
+    isMobileDevice: Boolean,
+    dark: Boolean,
+    windowWidth: Number,
+    windowHeight: Number,
+    drawer: Boolean
+  },
+  data: () => ({
+  }),
+  components: {
+  },
+  computed: {
+
+  },
+  watch: {
+  },
+  created() {
+    // this.currentUser = firebase.auth().currentUser;
+  },
+  methods: {
+ 
+  }
+}
+</script>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -184,15 +184,7 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/MintERC1155.vue'),
-      },
-      {
-        path: '/airdrop',
-        name: 'Air Drop',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Airdrop.vue'),
-      },
+      }
     ],
   },
 ];
